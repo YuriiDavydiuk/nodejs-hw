e8d37ec9395e91b77fd94f3864d2927327069a93
Yurii.Davydiuk <davydiuk.yurii@gmail.com>
2026-06-11 06:24:02 +0300

    Fix import path for TAGS in note.js

--- src/models/note.js
+++ src/models/note.js
@@ -1,6 +1,6 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
-import { TAGS } from '../constans/tags';
+import { TAGS } from '../constans/tags.js';
 
 const noteSchema = new Schema(
   {
